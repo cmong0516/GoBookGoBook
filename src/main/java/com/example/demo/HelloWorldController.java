ba745259788e121bb9d4228bf0803883cb6d36a6
cmong0516 <cmong0516@gmail.com>
2022-04-10 18:02:43 +0900

    BackTest

--- src/main/java/com/example/demo/HelloWorldController.java
+++ src/main/java/com/example/demo/HelloWorldController.java
@@ -1,6 +1,8 @@
 package com.example.demo;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Arrays;
@@ -12,5 +14,11 @@ public class HelloWorldController {
     @GetMapping("hello")
     public List<String> hello() {
         return Arrays.asList("안녕하세요", "Hello");
+    }
+
+    @PostMapping("/home")
+    public String home(@RequestParam("movieTitle") String movieTitle) {
+        System.out.println("movieTitle = " + movieTitle);
+        return movieTitle;
     }
 }
