602e72bf8b5fd341b9dbbd16c8cd8995d4c9bfa9
devpoooh <99088509+devpoooh@users.noreply.github.com>
2022-04-11 13:04:56 +0900

    test pooh

--- src/main/java/com/example/demo/HelloWorldController.java
+++ src/main/java/com/example/demo/HelloWorldController.java
@@ -19,6 +19,7 @@ public class HelloWorldController {
     @PostMapping("/home")
     public String home(@RequestParam("movieTitle") String movieTitle) {
         System.out.println("movieTitle = " + movieTitle);
+        System.out.println("잠시 빌릴게요...");
         return movieTitle;
     }
 }
