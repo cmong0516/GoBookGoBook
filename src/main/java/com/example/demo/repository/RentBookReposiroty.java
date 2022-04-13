539cc1b4a4384028c29d2cc6bfcd169b53acca17
cmong0516 <cmong0516@gmail.com>
2022-04-13 16:50:42 +0900

    rentBookRepository

---
+++ src/main/java/com/example/demo/repository/RentBookReposiroty.java
@@ -0,0 +1,20 @@
+package com.example.demo.repository;
+
+import com.example.demo.domain.RentBook;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface RentBookReposiroty {
+    RentBook save(RentBook rentBook);
+
+    RentBook findById(Long id);
+
+    Optional<RentBook> findByUserId(String userId);
+
+    List<RentBook> findAll();
+
+    Optional<RentBook> deleteOne(RentBook rentBook);
+}
