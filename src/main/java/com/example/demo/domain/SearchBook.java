591b4be8faba8ba6f37997fb1edc972c107784d1
devpoooh <huhu708@naver.com>
2022-04-13 21:57:47 +0900

    search 정보 뽑기 수정

---
+++ src/main/java/com/example/demo/domain/SearchBook.java
@@ -0,0 +1,17 @@
+package apitest.study.domain;
+
+import lombok.Data;
+
+@Data
+public class SearchBook {
+
+    private Long itemId;    //순서
+    private String title;   //책제목
+    private String contents; //줄거리
+    private String thumbnail; //커버사진 url
+    private String dateTime; //발행일
+    private String publisher; //카테고리
+    private String authors; //작가
+    private String translator;
+    private String isbn;
+}
