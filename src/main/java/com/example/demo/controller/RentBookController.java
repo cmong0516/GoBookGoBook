c7ac616640d052c8159e477a163097b0a64e4510
cmong0516 <cmong0516@gmail.com>
2022-04-18 00:14:54 +0900

    rentbookcontroller

--- src/main/java/com/example/demo/controller/RentBookController.java
+++ src/main/java/com/example/demo/controller/RentBookController.java
@@ -2,13 +2,25 @@ package com.example.demo.controller;
 
 import com.example.demo.domain.RentBook;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/rent")
 public class RentBookController {
 
-    @PostMapping("/rent")
-    public RentBook rentBook(RentBook rentBook) {
+    @PostMapping("/add")
+    public RentBook rentBookAdd(RentBook rentBook) {
+        rentBook.setTitle("테스트 타이틀");
+        rentBook.setAuthor("테스트 작가");
+        rentBook.setDescription("테스트 디스크립션");
+        return rentBook;
+    }
+
+    @GetMapping("/info")
+    public RentBook rentBookInfo() {
+        RentBook rentBook = new RentBook();
         rentBook.setTitle("테스트 타이틀");
         rentBook.setAuthor("테스트 작가");
         rentBook.setDescription("테스트 디스크립션");
