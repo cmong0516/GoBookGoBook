25cf7fbf65770790888ffa00034418141a86eb04
cmong0516 <cmong0516@gmail.com>
2022-04-18 11:32:58 +0900

    pathVariable

--- frontend/src/NewBooks.js
+++ frontend/src/NewBooks.js
@@ -1,31 +1,47 @@
-import React, {useEffect, useState} from 'react';
-import axios from 'axios';
-import Book from './Book.js'
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import Book from "./Book.js";
+import { Button, ButtonGroup, ButtonToolbar, Col } from "react-bootstrap";
 
 export let NewBooksContext = React.createContext();
 
 function NewBooks(props) {
+  let [books, setBooks] = useState();
+  let [menu, setMenu] = useState(0);
+  useEffect(() => {
+    axios
+      .get("/api/newbook")
+      .then((res) => {
+        setBooks(res.data);
+      })
+      .catch((error) => {
+        alert("도서 데이터를 받아오는 데 실패했습니다.");
+        console.log(error);
+      });
+  }, []);
 
-    let [books, setBooks] = useState();
-    
-    useEffect(() => {
-        axios.get('/api/newbook')
-            .then((res) => {
-                setBooks(res.data);
-            }).catch((error) => {
-                alert('도서 데이터를 받아오는 데 실패했습니다.');
-                console.log(error);
-            });
-    },[]);
-
-    return (
-        <div className='row'>
-            <NewBooksContext.Provider value={books}>
-                {props.getBooks(books)}
-                <Book />
-            </NewBooksContext.Provider>
-        </div>
-    )
+  return (
+    <div className="row">
+      <Col>
+        <ButtonToolbar aria-label="Toolbar with button groups">
+          <ButtonGroup className="me-2" aria-label="First group">
+            <Button>국내도서</Button>
+            <Button>국내소설</Button>
+            <Button>외국도서</Button>
+            <Button>4</Button>
+            <Button>5</Button>
+            <Button>6</Button>
+            <Button>7</Button>
+            <Button>8</Button>
+          </ButtonGroup>
+        </ButtonToolbar>
+        <NewBooksContext.Provider value={books}>
+          {props.getBooks(books)}
+          <Book />
+        </NewBooksContext.Provider>
+      </Col>
+    </div>
+  );
 }
 
 export default NewBooks;
