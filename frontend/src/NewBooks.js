bc1fe3017215fe746d5991aba60f1cf9cc1caefe
juaekoo <jool0129@gmail.com>
2022-04-18 17:02:49 +0900

    .

--- frontend/src/NewBooks.js
+++ frontend/src/NewBooks.js
@@ -29,11 +29,11 @@ function NewBooks(props) {
   function categoryNewbooks(categoryId) {
     axios.get('/api/newbook/'+ categoryId)
       .then((res) => {
-          alert('성공');
-          setBooks({
+          setBooks([...books, ...[]]);
+          setBooks([
             ...books,
             ...res.data
-          });
+          ]);
           console.log(books)
       }).catch((error) => {
           console.log(error)
@@ -42,30 +42,17 @@ function NewBooks(props) {
       });
   }
 
-  // 리렌더링
-  // useEffect(() => {
-  //   axios.get('/api/newbook/'+ categoryId)
-  //     .then((res) => {
-  //         alert('성공');
-  //         setBooks(res.data);
-  //     }).catch((error) => {
-  //         alert(categoryId)
-  //         alert('카테고리 신간 데이터를 받을 수 없습니다.');
-  //         console.log(error);
-  //     });
-  // },[categoryId]);
-
   return (
     <div className="row">
       <Col>
         <CategoryStyle>
           <Nav variant="tabs">
               <Nav.Item><Nav.Link onClick={() => {categoryNewbooks('101')}}>소설</Nav.Link></Nav.Item>
-              {/* <Nav.Item><Nav.Link onClick={()=>{ setCategoryId(102)}}>시/에세이</Nav.Link></Nav.Item> */}
-              <Nav.Item><Nav.Link eventKey="103">예술/대중문화</Nav.Link></Nav.Item>
-              <Nav.Item><Nav.Link eventKey="110">아동</Nav.Link></Nav.Item>
-              <Nav.Item><Nav.Link eventKey="117">경제경영</Nav.Link></Nav.Item>
-              <Nav.Item><Nav.Link eventKey="118">자기계발</Nav.Link></Nav.Item>
+              <Nav.Item><Nav.Link onClick={() => {categoryNewbooks('102')}}>시/에세이</Nav.Link></Nav.Item>
+              <Nav.Item><Nav.Link onClick={() => {categoryNewbooks('103')}}>예술/대중문화</Nav.Link></Nav.Item>
+              <Nav.Item><Nav.Link onClick={() => {categoryNewbooks('110')}}>아동</Nav.Link></Nav.Item>
+              <Nav.Item><Nav.Link onClick={() => {categoryNewbooks('117')}}>경제경영</Nav.Link></Nav.Item>
+              <Nav.Item><Nav.Link onClick={() => {categoryNewbooks('118')}}>자기계발</Nav.Link></Nav.Item>
           </Nav>
         </CategoryStyle>
 
