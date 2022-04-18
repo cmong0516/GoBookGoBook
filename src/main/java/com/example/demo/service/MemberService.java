1a9eeda442748ddb4e4d929ad263189dc053eae1
demian <wsji9404@gmail.com>
2022-04-18 15:08:50 +0900

    0418 15:08 wonseok

--- src/main/java/com/example/demo/service/MemberService.java
+++ src/main/java/com/example/demo/service/MemberService.java
@@ -5,18 +5,20 @@ import com.example.demo.repository.MemberRepository;
 import com.example.demo.repository.MemberRepositoryImpl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
 
 @Service
-//@Transactional 쓸지 말지. 정확히 이해 후 사용.
+@Transactional(readOnly = true) //쓸지 말지. 정확히 이해 후 사용.
 @RequiredArgsConstructor
 public class MemberService {
 
     private final MemberRepository memberRepository = new MemberRepositoryImpl();
 
     /*회원가입*/
+    @Transactional
     public Long join(Member member) {
         validateDuplicateMember(member);// 중복회원 검증
         memberRepository.save(member);
@@ -28,7 +30,7 @@ public class MemberService {
                 memberRepository.findByUserId(member.getUserId());
         if (!findMembers.isEmpty()) {
             throw new IllegalStateException("이미 존재하는 회원입니다");
-        }//이부분 List에서 Optional로 바꿈 꼬일수도 있음.
+        }
     }
 
     /*전체 회원 조회*/
