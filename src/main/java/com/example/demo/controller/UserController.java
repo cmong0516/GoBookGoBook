30e1ae9dbeabbce8a3d0a75b7b220f4781a096a0
devpoooh <huhu708@naver.com>
2022-04-22 20:55:40 +0900

    회원 조회

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -7,6 +7,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 
 @Slf4j
 @RestController
@@ -45,5 +47,10 @@ public class UserController {
 //        System.out.println("id = " + id); //확인
         rentService.deleteRent(user.getUserId());
         return userService.deleteUser(user.getUserId());
+    }
+
+    @GetMapping("/test")
+    public List<User> findAll(){
+        return userService.findAll();
     }
 }
