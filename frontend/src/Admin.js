46c6e054ab0b31f6a6eda3d288072460e1181a6a
juaekoo <jool0129@gmail.com>
2022-04-23 14:48:17 +0900

    Admin 디자인수정

--- frontend/src/Admin.js
+++ frontend/src/Admin.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { Col, Row, ListGroup, Button } from "react-bootstrap";
+import { Col, Row, ListGroup, Button, Table } from "react-bootstrap";
 import styled from "styled-components";
 import axios from "axios";
 
@@ -10,30 +10,32 @@ let MyMenu = styled.div`
   }
 `;
 let MyList = styled.div`
-  text-align: left;
   padding-left: 3rem;
   border-left: dashed 2px Silver;
 
-  h2 {
+  h3 {
+    text-align: left;
     margin-bottom: 1rem;
   }
 `;
+
 function Admin() {
+
   let [menu, setMenu] = useState(0);
   let [users, setUsers] = useState([]);
 
   useEffect(() => {
-    axios
-      .post("/all")
+    axios.post("/all")
       .then((res) => {
-        setUsers(res);
+        setUsers(res.data);
       })
       .catch((error) => {
         console.log(error);
       });
   }, []);
+
   return (
-    <div className="adminPage">
+    <div>
       <Row>
         <Col sm={3}>
           <MyMenu>
@@ -48,7 +50,11 @@ function Admin() {
         </Col>
         <Col sm={9}>
           <MyList>
-            {menu === 0 ? <AllUser users={users.data} /> : <AllRent />}
+            {
+              menu === 0 
+              ? <AllUser users={users} /> 
+              : <AllRent />
+            }
           </MyList>
         </Col>
       </Row>
@@ -59,39 +65,46 @@ function Admin() {
 export default Admin;
 
 function AllUser(props) {
+
   console.log(props.users);
+
   return (
-    <div className="allUser">
-      <table>
+    <div>
+      <h3>전체 회원관리</h3>
+      <Table striped hover>
         <thead>
-          <th>#</th>
-          <th>UserId</th>
-          <th>UserName</th>
-          <th>UserEmail</th>
-          <th>비밀번호 변경</th>
+          <tr>
+            <th>#</th>
+            <th>UserId</th>
+            <th>UserName</th>
+            <th>UserEmail</th>
+            <th>비밀번호 변경</th>
+          </tr>
         </thead>
         <tbody>
-          <td>props.users[0].id</td>
-          <td>props.users[0].userId</td>
-          <td>props.users[0].userName</td>
-          <td>props.users[0].userEmail</td>
-          <td className="adminButton">
-            {" "}
-            <Button variant="outline-info">초기화</Button>
-          </td>
+          <tr>
+            <td>props.users[0].id</td>
+            <td>props.users[0].userId</td>
+            <td>props.users[0].userName</td>
+            <td>props.users[0].userEmail</td>
+            <td>
+              <Button variant="outline-info" size="sm">초기화</Button>
+            </td>
+          </tr>
         </tbody>
-      </table>
+      </Table>
     </div>
   );
 }
 
 function AllRent(props) {
+
   let [rent, setRent] = useState([]);
+
   useEffect(() => {
-    axios
-      .post("/rent/infoall")
+    axios.post("/rent/infoall")
       .then((res) => {
-        setRent(res);
+        setRent(res.data);
       })
       .catch((error) => {
         console.log(error);
@@ -99,25 +112,30 @@ function AllRent(props) {
   }, []);
 
   return (
-    <div className="allRent">
-      <table>
+    <div>
+       <h3>전체 대여관리</h3>
+      <Table striped hover>
         <thead>
-          <th>RentId</th>
-          <th>UserId</th>
-          <th>BookTitle</th>
-          <th>RentDate</th>
-          <th>Change</th>
+          <tr>
+            <th>RentId</th>
+            <th>UserId</th>
+            <th>BookTitle</th>
+            <th>RentDate</th>
+            <th>Change</th>
+          </tr>
         </thead>
         <tbody>
-          <td>props.rent[0].rentId</td>
-          <td>props.rent[0].userId</td>
-          <td>props.rent[0].title</td>
-          <td>props.rent[0].rentDate</td>
-          <td className="adminButton">
-            <Button variant="outline-info">반납하기</Button>
-          </td>
+          <tr>
+            <td>props.rent[0].rentId</td>
+            <td>props.rent[0].userId</td>
+            <td>props.rent[0].title</td>
+            <td>props.rent[0].rentDate</td>
+            <td>
+              <Button variant="outline-info" size="sm">반납하기</Button>
+            </td>
+          </tr>
         </tbody>
-      </table>
+      </Table>
     </div>
   );
 }
