75ec7883f9d84806943229b1860c7c6bd4dfa923
juaekoo <jool0129@gmail.com>
2022-04-24 16:31:07 +0900

    관리자 : 대여/반납완료, 비번초기화/탈퇴 백엔드 로직 확인필요

--- frontend/src/Admin.js
+++ frontend/src/Admin.js
@@ -1,7 +1,8 @@
-import React, { useState, useEffect } from "react";
-import { Col, Row, ListGroup, Button, Table } from "react-bootstrap";
+import React, { useState } from "react";
+import { Col, Row, ListGroup } from "react-bootstrap";
 import styled from "styled-components";
-import axios from "axios";
+import AllUser from "./AllUser.js"
+import AllRent from "./AllRent.js"
 
 let MyMenu = styled.div`
   h4 {
@@ -45,114 +46,3 @@ function Admin() {
 }
 
 export default Admin;
-
-function AllUser(props) {
-  let [users, setUsers] = useState([]);
-
-  useEffect(() => {
-    axios
-      .post("/all")
-      .then((res) => {
-        console.log(res.data);
-        setUsers(res.data);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  }, []);
-  console.log(users);
-  return (
-    <div>
-      <h3>전체 회원관리</h3>
-      <Table striped hover>
-        <thead>
-          <tr>
-            <th>#</th>
-            <th>UserId</th>
-            <th>UserName</th>
-            <th>UserEmail</th>
-            <th>비밀번호 변경</th>
-            <th>회원탈퇴</th>
-          </tr>
-        </thead>
-        <tbody>
-          {users &&
-            users.map((user, i) => (
-              <tr>
-                <td>{user.id}</td>
-                <td>{user.userId}</td>
-                <td>{user.userName}</td>
-                <td>{user.userEmail}</td>
-                <td>
-                  <Button variant="outline-info" size="sm">
-                    초기화
-                  </Button>
-                </td>
-                <td>
-                  <Button variant="outline-danger" size="sm">
-                    회원 탈퇴
-                  </Button>
-                </td>
-              </tr>
-            ))}
-        </tbody>
-      </Table>
-    </div>
-  );
-}
-
-function AllRent(props) {
-  let [rent, setRent] = useState([]);
-  useEffect(() => {
-    axios
-      .post("/rent/infoall")
-      .then((res) => {
-        console.log(res);
-        setRent(res.data);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  }, []);
-
-  return (
-    <div>
-      <h3>전체 대여관리</h3>
-      <Table striped hover>
-        <thead>
-          <tr>
-            <th>RentId</th>
-            <th>UserId</th>
-            <th>BookTitle</th>
-            <th>RentDate</th>
-            <th>반납상태</th>
-            <th>#</th>
-          </tr>
-        </thead>
-        <tbody>
-          {rent &&
-            rent.map((rent, i) => (
-              <tr>
-                <td>{rent.rentId}</td>
-                <td>{rent.userId}</td>
-                <td>{rent.title}</td>
-                <td>{rent.rentDate}</td>
-                <td>{rent.state == true ? "대여중" : "반납완료"}</td>
-                <td>
-                  {rent.state == true ? (
-                    <Button variant="outline-info" size="sm">
-                      반납하기
-                    </Button>
-                  ) : (
-                    <Button variant="outline-success" size="sm">
-                      대여하기
-                    </Button>
-                  )}
-                </td>
-              </tr>
-            ))}
-        </tbody>
-      </Table>
-    </div>
-  );
-}
