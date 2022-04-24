15202c331b042c51c463be87f7f9b1913bb2075a
juaekoo <jool0129@gmail.com>
2022-04-24 13:04:30 +0900

    관리자아이디(Admin0)으로 로그인 시 상단메뉴에 관리자페이지(현재 주석처리)

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -61,38 +61,51 @@ function App() {
             </a>
           </div>
           <div>
-            {userId ? (
-              <div>
-                <Link to="/">
-                  <span
-                    onClick={() => {
-                      localStorage.clear();
-                      setIsLogin(false); // 얘를 주석처리하면 로그아웃 눌러도 로그인으로 돌아오지 않음
-                    }}
-                  >
-                    로그아웃
-                  </span>
-                </Link>
-                <span>|</span>
-                <Link to="/mypage">
-                  <span>마이페이지&nbsp;</span>
-                </Link>
-                <span>|</span>
-                <Link to="/admin">
-                  <span>Admin</span>
-                </Link>
-              </div>
-            ) : (
-              <div>
-                <Link to="/login">
-                  <span>로그인</span>
-                </Link>
-                <span>|</span>
-                <Link to="/signin">
-                  <span>회원가입</span>
-                </Link>
-              </div>
-            )}
+            {
+              userId
+                ? (
+                  <div>
+                    <Link to="/">
+                      <span
+                        onClick={() => {
+                          localStorage.clear();
+                          setIsLogin(false); // 얘를 주석처리하면 로그아웃 눌러도 로그인으로 돌아오지 않음
+                        }}
+                      >
+                        로그아웃
+                      </span>
+                    </Link>
+                    <span>|</span>
+                    <Link to="/mypage">
+                      <span>마이페이지&nbsp;</span>
+                    </Link>
+                    <span>|</span>
+                    <Link to="/admin">
+                      <span>Admin</span>
+                    </Link>
+                    {/* {
+                      userId == 'admin0'
+                      ? (<Link to="/admin">
+                        <span>관리자페이지</span>
+                        </Link>)
+                      : (<Link to="/mypage">
+                        <span>마이페이지&nbsp;</span>
+                        </Link>)
+                    } */}
+                  </div>
+                )
+                : (
+                  <div>
+                    <Link to="/login">
+                      <span>로그인</span>
+                    </Link>
+                    <span>|</span>
+                    <Link to="/signin">
+                      <span>회원가입</span>
+                    </Link>
+                  </div>
+                )
+            }
           </div>
         </div>
         <SearchBar
