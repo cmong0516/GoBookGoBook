6f619dbe0135355610af92e69132a1ef4bcdfbf8
juaekoo <jool0129@gmail.com>
2022-04-25 17:02:49 +0900

    관리자테이블 디자인수정

--- frontend/src/book/Search.js
+++ frontend/src/book/Search.js
@@ -25,73 +25,50 @@ function Search(props) {
   }, [searchWord, stateCheck]);
 
   return (
-    <div className="searchPage">
-      {/* 검색결과가 없는 경우도 처리해야함 */}
-      <Row xs={1} md={4} className="g-4">
-        {result &&
-          result.map((book, i) => (
-            <Col>
-              <Card className="searchcard">
-                <Badge bg="primary" className="searchindex">
-                  {i + 1}
-                </Badge>
-                <Card.Img
-                  variant="top"
-                  src={book.thumbnail}
-                  className="cardImg"
-                />
-                <Card.Body>
-                  <Card.Title>{book.title.substr(0, 15)}...</Card.Title>
-                  <Card.Text>{book.authors}</Card.Text>
-                  <Card.Text>{book.publisher}</Card.Text>
-                  <Card.Text>{book.contents.substr(0, 60)}...</Card.Text>
-                </Card.Body>
-              </Card>
-              {userId != "admin0" ? (
-                <RentButton
-                  book={{
-                    author: book.authors,
-                    categoryName: book.categoryName,
-                    coverLargeUrl: book.coverLargeUrl,
-                    coverSmallUrl: book.thumbnail,
-                    customerReviewRank: book.customerReviewRank,
-                    description: book.contents,
-                    isbn: book.isbn,
-                    pubDate: book.dateTime,
-                    publisher: book.publisher,
-                    rank: book.rank,
-                    title: book.title,
-                    userId: userId,
-                  }}
-                  stateCheck={stateCheck}
-                  setStateCheck={setStateCheck}
-                />
-              ) : null}
-            </Col>
-          ))}
-      </Row>
-      {/* 여기까지 html */}
-      {/* books state를 만들어 버튼에 넣으면 이미 books값을 가져오기 전에 RentButton으로 넘어가버려서 undefined가 출력되는 것으로 추정
-
-                    {() => setBooks({
-                        author: book.authors,
-                        categoryName: book.categoryName,
-                        coverLargeUrl: book.coverLargeUrl,
-                        coverSmallUrl: book.thumbnail,
-                        customerReviewRank: book.customerReviewRank,
-                        description: book.contents,
-                        isbn: book.isbn,
-                        pubDate: book.dateTime,
-                        publisher: book.publisher,
-                        rank: book.rank,
-                        // title: book.title,
-                        title: "체크",
-                        userId: userId
-                    })} 
-                    
-                    <RentButton book={books} />*/}
-    </div>
-  );
+    <Row xs={1} md={4} className="g-4">
+      {result &&
+        result.map((book, i) => (
+          <Col>
+            <Card className="searchcard">
+              <Badge bg="primary" className="searchindex">
+                {i + 1}
+              </Badge>
+              <Card.Img
+                variant="top"
+                src={book.thumbnail}
+                className="cardImg"
+              />
+              <Card.Body>
+                <Card.Title>{book.title.substr(0, 15)}...</Card.Title>
+                <Card.Text>{book.authors}</Card.Text>
+                <Card.Text>{book.publisher}</Card.Text>
+                <Card.Text>{book.contents.substr(0, 60)}...</Card.Text>
+              </Card.Body>
+            </Card>
+            {userId != "admin0" ? (
+              <RentButton
+                book={{
+                  author: book.authors,
+                  categoryName: book.categoryName,
+                  coverLargeUrl: book.coverLargeUrl,
+                  coverSmallUrl: book.thumbnail,
+                  customerReviewRank: book.customerReviewRank,
+                  description: book.contents,
+                  isbn: book.isbn,
+                  pubDate: book.dateTime,
+                  publisher: book.publisher,
+                  rank: book.rank,
+                  title: book.title,
+                  userId: userId,
+                }}
+                stateCheck={stateCheck}
+                setStateCheck={setStateCheck}
+              />
+            ) : null}
+          </Col>
+        ))}
+    </Row>
+  )
 }
 
 export default Search;
