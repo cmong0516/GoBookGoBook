4a431224ee7edfb16bd70adaa719fe04d3fcda9f
juaekoo <jool0129@gmail.com>
2022-04-27 16:05:47 +0900

    사용하지 않는 hook 제거

--- frontend/src/user/Login.js
+++ frontend/src/user/Login.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import React, { useState, useContext } from "react";
+import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 import { useHistory, Link } from "react-router-dom";
 import styled from "styled-components";
